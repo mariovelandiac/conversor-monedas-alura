2e096a274633295fe96a73489ff31885bedee093
mariovelandiac <mariovelandiawork@gmail.com>
2023-07-20 19:55:30 -0500

    ✖️ imports sin uso

--- conversor-monedas/src/converter/models/MenuInicialModel.java
+++ conversor-monedas/src/converter/models/MenuInicialModel.java
@@ -1,11 +1,8 @@
 package converter.models;
 
-import java.io.IOException;
-
 import converter.constants.InfoText;
 import converter.constants.OpcionesConversor;
 import converter.constants.OptionList;
-import converter.strategies.ConversorStrategy;
 
 public class MenuInicialModel extends Model {
 	
